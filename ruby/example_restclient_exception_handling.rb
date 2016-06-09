b18e96dd068c969e18e60b2d5f12f385aa6cb8ba
Jeffrey Her <her.jeff.5@gmail.com>
2016-06-09 13:29:28 -0700

    [update] Fixed indentations.

--- ruby/example_restclient_exception_handling.rb
+++ ruby/example_restclient_exception_handling.rb
@@ -24,24 +24,24 @@ RestClient.log = $stdout
   },
   :payload => {
     royalty_report_items: [
-        {
-            brand:              "ASUS",
-            model:              "90-YAA055-1UAN00Z",
-            technology_num:     "HDMI",
-            qty:                254,
-            unit_price:         190.50,
-            royalty_due:        190.50,
-            qty_technology:     1,
-        }
+      {
+        brand:              "ASUS",
+        model:              "90-YAA055-1UAN00Z",
+        technology_num:     "HDMI",
+        qty:                254,
+        unit_price:         190.50,
+        royalty_due:        190.50,
+        qty_technology:     1,
+      }
     ]
   }.to_json)
 
 @signed_request = ApiAuth.sign!(@request, @access_id, @secret_key, :with_http_method => true)
 
 begin
-    response = @signed_request.execute
-    $stdout.print response.to_s + "\n"
+  response = @signed_request.execute
+  $stdout.print response.to_s + "\n"
 rescue => bad_request
-    response = JSON.parse(bad_request.response)
-    $stdout.print("#{bad_request.message}: #{response["error"]}\n")
+  response = JSON.parse(bad_request.response)
+  $stdout.print("#{bad_request.message}: #{response["error"]}\n")
 end
